3c1d889cfc57665f4094819dc0f7bd6575013ddf
Mateus Rodrigues de Souza <mateusrdssouza@gmail.com>
2025-02-05 22:25:08 -0300

    chore: add bycript

---
+++ backend/src/utils/bcrypt.ts
@@ -0,0 +1,8 @@
+import * as bcrypt from 'bcrypt';
+
+export async function encryptPassword(password: string): Promise<string> {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  return hashedPassword;
+}
