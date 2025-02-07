19e782ef33c801eb83726dcecc15ef849b38d0f8
Mateus Rodrigues de Souza <mateusrdssouza@gmail.com>
2025-02-07 00:40:22 -0300

    fix: jest config

---
+++ backend/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testRegex: '.*\\.spec\\.ts$',
+};
